cd760342027fe1665effabc1a3999dca6e7aa4d6
Aleksei Bljahhin <aleksei.bljahhin@gmail.com>
2025-12-17 09:27:28 +0000

    fix(ci): consolidate frontend jest configuration (#72)
    Co-authored-by: Aleksei Bljahhin <aleksei.bljahhiin@gmail.com>

--- packages/frontend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-  }
-};
