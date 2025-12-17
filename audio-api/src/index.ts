c06dd7d89fc6fd6f145998b1122e2cf3cc50a4ba
Aleksei Bljahhin <aleksei.bljahhin@gmail.com>
2025-12-17 13:06:11 +0000

    fix: add explicit region to S3 and SQS clients (#117)
    Co-authored-by: Aleksei Bljahhin <aleksei.bljahhiin@gmail.com>

--- audio-api/src/index.ts
+++ audio-api/src/index.ts
@@ -2,8 +2,9 @@ import { S3Client } from '@aws-sdk/client-s3';
 import { SQSClient } from '@aws-sdk/client-sqs';
 import { handler } from './handler';
 
-const s3Client = new S3Client({});
-const sqsClient = new SQSClient({});
+const region = process.env.AWS_REGION || 'eu-west-1';
+const s3Client = new S3Client({ region });
+const sqsClient = new SQSClient({ region });
 
 export async function lambdaHandler(event: any): Promise<any> {
   const response = await handler(event, s3Client, sqsClient);
