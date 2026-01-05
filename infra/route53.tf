cfae2458a091bdff5a05a57cdf89de032c54f042
Aleksei Bljahhin <aleksei.bljahhiin@gmail.com>
2026-01-05 16:39:05 +0000

    fix: enable cert_validation DNS records and point to CloudFront

--- infra/route53.tf
+++ infra/route53.tf
@@ -1,31 +1,30 @@
-# DNS record for website - TEMPORARY: pointing to S3 website endpoint
-# Will switch to CloudFront after AWS account verification
+# DNS record for website - pointing to CloudFront
 resource "aws_route53_record" "website" {
   zone_id = data.terraform_remote_state.infra.outputs.route53_zone_id
   name    = local.domain_name
   type    = "A"
 
   alias {
-    name                   = aws_s3_bucket_website_configuration.website.website_domain
-    zone_id                = aws_s3_bucket.website.hosted_zone_id
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
     evaluate_target_health = false
   }
 }
 
-# # DNS validation records for ACM certificate - DISABLED temporarily
-# resource "aws_route53_record" "cert_validation" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.website.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     }
-#   }
+# DNS validation records for ACM certificate
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.website.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.record]
-#   ttl             = 60
-#   type            = each.value.type
-#   zone_id         = data.terraform_remote_state.infra.outputs.route53_zone_id
-# }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.terraform_remote_state.infra.outputs.route53_zone_id
+}
